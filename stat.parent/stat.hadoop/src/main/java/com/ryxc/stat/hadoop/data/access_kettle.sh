e0a618295533b68575c91271bd0933e13a40f2e6
tonye0115 <tonye0115>
2016-10-05 12:22:00 +0800

    提交kettle脚本

--- stat.parent/stat.hadoop/src/main/java/com/ryxc/stat/hadoop/data/access_kettle.sh
+++ stat.parent/stat.hadoop/src/main/java/com/ryxc/stat/hadoop/data/access_kettle.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
-source /etc/profile
+source ~/.bash_profile
 stat_date=`date -d "yesterday" +"%Y-%m-%d"`
 
 echo "job begin "
 echo `date +"%Y-%m-%d %H:%M:%S"`
-cd /home/ryxc/work
+echo "stat_data"${stat_date}
+start_time=`date +%s`
 
 sh /home/ryxc/kettle/data-integration/kitchen.sh -rep file_rep -job J_O_access_stat -dir /access -param:stat_date=${stat_date} -level Detailed > /home/ryxc/work/stat/logs/J_O_access_stat.log.${stat_date} 2>&1
 
-echo "job end "
+end_time=`date +%s`
+timeInteval=$[ end_time - start_time ]
+echo "timeInteval-"$timeInteval" m"
 echo `date +"%Y-%m-%d %H:%M:%S"`
+echo "job end "
+echo "============================================================"
